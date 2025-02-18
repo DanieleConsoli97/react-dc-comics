dd1f675515292d67705bc72f942ca218c368037d
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-02-18 14:58:02 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+
+function Header() {
+   return(
+    <div className="nav_bar">
+        <div className="img">
+          <img src="./assets/dc-logo.png" alt="logo.png" />
+        </div>
+        <div className="link">
+            <ul>
+                <li>characters</li>
+                <li>comics</li>
+                <li>movies</li>
+                <li>tv</li>
+                <li>games</li>
+                <li>collectbles</li>
+                <li>videos</li>
+                <li>fans</li>
+                <li>news</li>
+                <li>shop</li>
+            </ul>
+        </div>
+    </div>
+   ) 
+    
+}
+export default Header
