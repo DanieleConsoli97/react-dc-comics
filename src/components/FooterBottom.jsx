daace516aff8cede11464308182b4aec31926b8a
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-02-20 15:34:51 +0100

    add social js

--- src/components/FooterBottom.jsx
+++ src/components/FooterBottom.jsx
@@ -1,7 +1,17 @@
 function FooterBottom (){
     return(
         <div className="footer_bottom">
-                    
+            <div> <button>sing-up now</button></div>
+            <div>
+                <ul className="footer_bottom_social">
+                    <li className="social"><img src="./img/footer-facebook.png" alt="" /></li>
+                    <li><img className="social" src="./img/footer-periscope.png" alt="" /></li>
+                    <li><img className="social" src="./img/footer-pinterest.png" alt="" /></li>
+                    <li><img className="social" src="./img/footer-twitter.png" alt="" /></li>
+                    <li><img className="social" src="./img/footer-youtube.png" alt="" /></li>
+                </ul>
+            </div>
+           
                 </div>
     )
 
---
+++ src/data/social.js
@@ -0,0 +1,7 @@
+const social= [
+    {img:"./img/footer-facebook.png"},
+    {img:"./img/footer-periscope.png"},
+    {img:"./img/footer-pinterest.png"},
+    {img:"./img/footer-twitter.png"},
+    {img:"./img/footer-youtube.png"}
+]
