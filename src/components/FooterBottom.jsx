ef093750110abac0647bb026f233386f581910ff
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-02-20 15:48:07 +0100

    end

--- src/components/FooterBottom.jsx
+++ src/components/FooterBottom.jsx
@@ -2,14 +2,14 @@ function FooterBottom (props){
     const {social} = props; 
     return(
         <div className="footer_bottom">
-            <div> <button>sing-up now</button></div>
+           <button>sing-up now</button>
             <div>
                 <ul className="footer_bottom_social">
                     {
                         social.map((element)=>{
-                            const {img} = element;
+                            const {id,img} = element;
                             return (
-                                <li className="social"><img src={img} alt="" /></li>
+                                <li key={id} className="social"><img src={img} alt="" /></li>
                             )
                         })
 
--- src/data/social.js
+++ src/data/social.js
@@ -1,8 +1,8 @@
 const social= [
-    {img:"./img/footer-facebook.png"},
-    {img:"./img/footer-periscope.png"},
-    {img:"./img/footer-pinterest.png"},
-    {img:"./img/footer-twitter.png"},
-    {img:"./img/footer-youtube.png"}
+    {id:1,img:"./img/footer-facebook.png"},
+    {id:2,img:"./img/footer-periscope.png"},
+    {id:3,img:"./img/footer-pinterest.png"},
+    {id:4,img:"./img/footer-twitter.png"},
+    {id:5,img:"./img/footer-youtube.png"}
 ]
 export default social;
